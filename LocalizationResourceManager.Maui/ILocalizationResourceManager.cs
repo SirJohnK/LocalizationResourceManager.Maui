53bf1874a77216db506b83f5c29d5532a059ba5e
Johan Svensson <Johan.Svensson@strongpoint.com>
2022-12-29 00:22:47 +0100

    Add Indexer Operator in ILocalizationResourceManager

--- LocalizationResourceManager.Maui/ILocalizationResourceManager.cs
+++ LocalizationResourceManager.Maui/ILocalizationResourceManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace LocalizationResourceManager.Maui
 {
@@ -14,5 +9,7 @@ namespace LocalizationResourceManager.Maui
         CultureInfo CurrentCulture { get; set; }
 
         string GetValue(string text);
+
+        string this[string text] { get; }
     }
 }
