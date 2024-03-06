0bb69e361540a71ce4756373cb11563d72012313
trdsolutions <info@tr-dieckmann.de>
2024-03-06 19:16:16 +0100

    Added example of usage for TranslateExtension with IValueConverter

---
+++ LocalizationResourceManager.Maui.Sample/Converters/ToUpperConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace LocalizationResourceManager.Maui.Sample.Converters;
+
+public class ToUpperConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        (value as string ?? value?.ToString())?.ToUpper(culture);
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
